dfbe11ade8da0293ed8d7e095deec2072c63468c
Kumaraswamy Vithanala <36370570+kumarvna@users.noreply.github.com>
2022-10-29 14:22:13 +0530

    main configuration update

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,9 +3,15 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_log_analytics_workspace" "example" {
+  name                = "loganalytics-we-sharedtest2"
+  resource_group_name = "rg-shared-westeurope-01"
+}
+
 module "firewall" {
-  source  = "kumarvna/firewall/azurerm"
-  version = "1.1.0"
+  // source  = "kumarvna/firewall/azurerm"
+  // version = "1.1.0"
+  source = "../../"
 
   # By default, this module will not create a resource group. Location will be same as existing RG.
   # proivde a name to use an existing resource group, specify the existing resource group name, 
@@ -78,9 +84,9 @@ module "firewall" {
     },
   ]
 
-  # (Optional) To enable Azure Monitoring for Azure MySQL database
-  # (Optional) Specify `storage_account_name` to save monitoring logs to storage. 
-  log_analytics_workspace_name = "loganalytics-we-sharedtest2"
+  # (Optional) To enable Azure Monitoring and diagnostics to firewall and public ip's
+  # (Optional) Specify `storage_account_name` to save monitoring logs to storage.   
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.example.id
 
   # Adding TAG's to your Azure resources 
   tags = {
